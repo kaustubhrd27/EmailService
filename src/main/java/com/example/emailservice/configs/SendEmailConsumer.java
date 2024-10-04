a806437b212d2ca3f05d3a4eb37082dc32af0f75
DELL <kaustubhrd27@gmail.com>
2024-10-04 21:29:03 +0530

    Initial commit

---
+++ src/main/java/com/example/emailservice/configs/SendEmailConsumer.java
@@ -0,0 +1,65 @@
+package com.example.emailservice.configs;
+
+import com.example.emailservice.dto.SendEmailDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import java.util.Properties;
+
+@Service
+public class SendEmailConsumer {
+    private ObjectMapper objectMapper;
+    private EmailUtil emailUtil;
+    public SendEmailConsumer(ObjectMapper objectMapper, EmailUtil emailUtil) {
+
+        this.objectMapper = objectMapper;
+        this.emailUtil = emailUtil;
+    }
+
+    @KafkaListener(topics = "sendEmail", groupId = "emailService")
+    public void handleSendEmailEvent(String message) {
+        // this should be triggered when there is call when this type of topic(sendEmail) is hit
+        // code to send email to the user
+
+        SendEmailDto sendEmailDto = null;
+        try {
+            sendEmailDto = objectMapper.readValue(message, SendEmailDto.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Send an Email.
+        //SMTP -> Simple Mail Transfer Protocol.
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+        props.put("mail.smtp.port", "587"); //TLS Port
+        props.put("mail.smtp.auth", "true"); //enable authentication
+        props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+
+        //create Authenticator object to pass in Session.getInstance argument
+        Authenticator auth = new Authenticator() {
+            //override the getPasswordAuthentication method
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("temp39766@gmail.com", "wddohrzomjejssjd");
+            }
+        };
+        Session session = Session.getInstance(props, auth);
+
+        emailUtil.sendEmail(
+                session,
+                sendEmailDto.getTo(),
+                sendEmailDto.getSubject(),
+                sendEmailDto.getBody()
+        );
+
+
+
+
+
+    }
+}
